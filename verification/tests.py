2021c2f3998215fad8730891a1370bcc169ea9e0
Oleksandr Zozulia <freemanlex@gmail.com>
2025-09-18 21:41:18 +0300

    mission creation

--- verification/tests.py
+++ verification/tests.py
@@ -11,26 +11,102 @@ Each test is a dict with
 TESTS = {
     "Basics": [
         {
-            "input": [3, 2],
-            "answer": 5,
-            "explanation": "3+2=?"
+            "input": ["H"],
+            "answer": "1s1"
         },
         {
-            "input": [5, 7],
-            "answer": 12,
-            "explanation": "5+7=?"
+            "input": ["He"],
+            "answer": "1s2"
+        },
+        {
+            "input": ["Al"],
+            "answer": "[Ne] 3s2 3p1"
+        },
+        {
+            "input": ["O"],
+            "answer": "[He] 2s2 2p4"
+        },
+        {
+            "input": ["Li"],
+            "answer": "[He] 2s1"
         },
     ],
     "Extra": [
         {
-            "input": [6, 3],
-            "answer": 9,
-            "explanation": "6+3=?"
+            "input": ["Uuo"],
+            "answer": "[Rn] 5f14 6d10 7s2 7p6"
         },
         {
-            "input": [6, 7],
-            "answer": 13,
-            "explanation": "6+7=?"
+            "input": ["In"],
+            "answer": "[Kr] 4d10 5s2 5p1"
         },
+        {
+            "input": ["I"],
+            "answer": "[Kr] 4d10 5s2 5p5"
+        },
+        {
+            "input": ["Ir"],
+            "answer": "[Xe] 4f14 5d7 6s2"
+        },
+        {
+            "input": ["Tl"],
+            "answer": "[Xe] 4f14 5d10 6s2 6p1"
+        },
+        {
+            "input": ["Db"],
+            "answer": "[Rn] 5f14 6d3 7s2"
+        },
+        {
+            "input": ["V"],
+            "answer": "[Ar] 3d3 4s2"
+        },
+        {
+            "input": ["Rn"],
+            "answer": "[Xe] 4f14 5d10 6s2 6p6"
+        },
+        {
+            "input": ["Xe"],
+            "answer": "[Kr] 4d10 5s2 5p6"
+        },
+        {
+            "input": ["Br"],
+            "answer": "[Ar] 3d10 4s2 4p5"
+        },
+        {
+            "input": ["Na"],
+            "answer": "[Ne] 3s1"
+        },
+        {
+            "input": ["Kr"],
+            "answer": "[Ar] 3d10 4s2 4p6"
+        },
+        {
+            "input": ["K"],
+            "answer": "[Ar] 4s1"
+        },
+        {
+            "input": ["Ds"],
+            "answer": "[Rn] 5f14 6d8 7s2"
+        },
+        {
+            "input": ["Ar"],
+            "answer": "[Ne] 3s2 3p6"
+        },
+        {
+            "input": ["Rb"],
+            "answer": "[Kr] 5s1"
+        },
+        {
+            "input": ["Uus"],
+            "answer": "[Rn] 5f14 6d10 7s2 7p5"
+        },
+        {
+            "input": ["Cl"],
+            "answer": "[Ne] 3s2 3p5"
+        },
+        {
+            "input": ["Fr"],
+            "answer": "[Rn] 7s1"
+        }
     ]
 }
